bad8d0a51d2cfc30148b5b7cef5e492a87954881
ALEXANDER <belligham.gordillo05@gmail.com>
2026-05-02 15:35:43 -0500

    v1.clases.py

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,32 +1,18 @@
 from aircraft import Aircraft
 
-try:
-    model = input()
-except EOFError:
-    model = ""
-
+model = input()
 aircraft = Aircraft(model)
 
 while True:
-    try:
-        command = input()
-    except EOFError:
-        break
+    command = input()
 
     if command == "X":
         break
 
     parts = command.split()
 
-    if len(parts) != 2:
-        continue
-
-    action, value = parts[0], parts[1]
-
-    try:
-        value = int(value)
-    except:
-        continue
+    action = parts[0]
+    value = int(parts[1])
 
     if action == "A":
         aircraft.ascend(value)
